c7bc1632a566eb925098d3c18513d331b909bafc
C3NZ <epichdd@gmail.com>
2017-12-05 19:40:46 -0500

    Arrays challenge

---
+++ day8.py
@@ -0,0 +1,22 @@
+entries = int(input())
+
+phonebook = {}
+for i in range(entries):
+    entry = input().split()
+    phonebook[entry[0]] = entry[1]
+
+while True:
+    person = None
+    
+    try:
+        person = input()
+    except EOFError:
+        break
+        
+    if person == "":
+        break
+    
+    if person in phonebook.keys():
+        print(person + "=" + phonebook[person])
+    else:
+        print("Not found")
